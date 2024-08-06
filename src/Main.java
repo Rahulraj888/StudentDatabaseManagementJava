369a9cd505a0d7680429b6062f1af093d441e838
Rahulraj888 <reddavenirahul@gmail.com>
2024-08-05 22:33:47 -0400

    minor change

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import view.MainView;
 import controller.StudentController;
 import view.AddStudentView;
 
+/**
+ * starting point of the project
+ * renders the main view and initializes the other classes as well
+ */
 public class Main {
     public static void main(String[] args) {
         MainView mainView = new MainView(); //setup main view
